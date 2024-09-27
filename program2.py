09e510b6614c4eb4c204ed4b9cc8232cbf2eef54
taruna-likhitkar <tarunalikhitkar@gmail.com>
2024-09-27 12:51:30 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,16 @@
 def decode_message( s: str, p: str) -> bool:
-
-# write your code here
+  m,n = len(s),len(p)
+  dp = [[False]*(n+1) for _ in range(m+1)]
+  dp[0][0] = True
+  for j in range (1,n+1):
+    if p[j-1] == '*':
+      dp[0][j] = dp[0][j-1]
+  for i in range (1,m+1):
+    for j in range (1,n+1):
+      if p[j-1] == '?' or p[j-1] == s[i-1]:
+        dp[i][j] = dp[i-1][j-1]
+      elif p[j-1] == '*':
+        dp[i][j] = dp[i][j-1] or dp[i-1][j]
+  return dp[m][n]
   
-        return False
+  
